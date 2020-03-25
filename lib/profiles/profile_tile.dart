239aa6705c3602e4d347a38f62c696eeee3db839
Joshua Jarvis <joshuajarvis0711@gmail.com>
2020-03-24 22:51:21 -0600

    Changes to avoid nested scrolling

--- lib/profiles/profile_tile.dart
+++ lib/profiles/profile_tile.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import '../resources/text.dart';
 import '../resources/theme.dart';
 
 class ProfileTile extends StatefulWidget{
-  String imgPth;
-  String profileName;
-  String profileText;
+  final String imgPth;
+  final String profileName;
+  final String profileText;
+
   ProfileTile(Key key, this.imgPth, this.profileName, this.profileText) : super(key: key);
   @override
   _ProfileTileState createState() => _ProfileTileState(this.imgPth, this.profileName, this.profileText);
@@ -16,15 +16,65 @@ class _ProfileTileState extends State<ProfileTile> {
   String imgPth;
   String profileName;
   String profileText;
-  double mobileWidth = 570;
   double shadowOffset;
-  double imgDimension = 200;
+  final double mobileWidth = 570;
+  final double mobileHeight = 250;
   _ProfileTileState(this.imgPth, this.profileName, this.profileText) : super();
 
   Widget build(BuildContext build){
     var mediaData = MediaQuery.of(build);
     var width = mediaData.size.width;
     var titleAlignment = this.mobileWidth < width ? Alignment.topLeft : Alignment.center;
+
+    return MouseRegion(
+      onEnter: this.grow,
+      onExit: this.shrink,
+
+      child: Container(
+        height: 400,
+        margin: EdgeInsets.all(WebsiteTheme.profileMargin),
+        padding: EdgeInsets.all(WebsiteTheme.profilePadding),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20)
+          ),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 1.0,
+              offset: Offset(
+                this.shadowOffset,
+                this.shadowOffset,
+              ),
+            )
+          ],
+          color: WebsiteTheme.backgroundColor
+        ),
+        child: Column(
+          children: <Widget>[
+            Align(
+              alignment: titleAlignment,
+              child: Text(
+                profileName,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: WebsiteTheme.secondaryTextColor
+                ),
+              )
+            ),
+            SizedBox(height: 15),
+            this.getProfileContents(build)
+          ]
+        ),
+      ),
+    );
+  }
+
+  Widget getProfileContents(BuildContext build){
+    double imgDimensionLandscape = 200;
+    double imgDimensionPortrait = 300;
+    var mediaData = MediaQuery.of(build);
+    var width = mediaData.size.width;
+    var height = mediaData.size.height;
 
     Widget profileContents;
 
@@ -35,8 +85,8 @@ class _ProfileTileState extends State<ProfileTile> {
             alignment: Alignment.topLeft,
             child: Image.asset(
               this.imgPth,
-              width: this.imgDimension,
-              height: this.imgDimension
+              width: imgDimensionLandscape,
+              height: imgDimensionLandscape
             ),
           ),
           SizedBox(width: 15),
@@ -56,92 +106,37 @@ class _ProfileTileState extends State<ProfileTile> {
     }
 
     else {
-      profileContents = Column(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.center,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5.0),
-              child: Image.asset(
-                imgPth,
-                width: this.imgDimension,
-                height: this.imgDimension
-              ),
-            )
+      profileContents = Align(
+        alignment: Alignment.center,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(5.0),
+          child: Image.asset(
+            imgPth,
+            width: imgDimensionPortrait,
+            height: imgDimensionPortrait
           ),
-          SizedBox(height: 15),
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              profileText,
-              style: TextStyle(
-                color: WebsiteTheme.secondaryTextColor
-              )
-            )
-          )
-        ],
+        ),
       );
     }
 
-    return MouseRegion(
-      onEnter: this.grow,
-      onExit: this.shrink,
-
-      child: Container(
-        height: 400,
-        margin: EdgeInsets.all(WebsiteTheme.profileMargin),
-        padding: EdgeInsets.all(WebsiteTheme.profilePadding),
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              blurRadius: this.shadowOffset,
-              offset: Offset(
-                10.0,
-                10.0,
-              ),
-            )
-          ],
-          borderRadius: BorderRadius.all(
-            Radius.circular(20)
-          ),
-          color: WebsiteTheme.backgroundColor
-        ),
-        child: ListView(
-          physics: ClampingScrollPhysics(),
-          children: <Widget>[
-            Align(
-              alignment: titleAlignment,
-              child: Text(
-                profileName,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: WebsiteTheme.secondaryTextColor
-                ),
-              )
-            ),
-            SizedBox(height: 15),
-            profileContents
-          ]
-        ),
-      ),
-    );
+    return profileContents;
   }
 
   @override
   void initState() {
-    this.shadowOffset = 20.0;
+    this.shadowOffset = 10.0;
     super.initState();
   }
 
   void grow(PointerEnterEvent e){
     setState(() {
-      this.shadowOffset = 30.0;
+      this.shadowOffset = 15.0;
     });
   }
 
   void shrink(PointerExitEvent e){
     setState(() {
-      this.shadowOffset = 20.0;
+      this.shadowOffset = 10.0;
     });
   }
 }
